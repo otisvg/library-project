ff3ce26931a5d07e932c80f3a8c966b55dad9a08
Otis <otisvickersgraver@gmail.com>
2021-01-19 21:23:36 +0000

    connect to mongodb cluster

--- server/index.js
+++ server/index.js
@@ -2,3 +2,19 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';  
+import dotenv from 'dotenv';
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json({ limit: "20mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "20mb", extended: true }));
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONN_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((err) => console.log(err.message)); 
+    
+mongoose.set('useFindAndModify', false);
