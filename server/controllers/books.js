4c842bd5e67a848be46c6bc1950363f393ffdd56
Otis <otisvickersgraver@gmail.com>
2021-02-09 23:59:52 +0000

    createBook route

--- server/controllers/books.js
+++ server/controllers/books.js
@@ -9,3 +9,15 @@ export const getBooks = async (req, res) => {
      res.status(404).json({ message: err.message });
    }
 }
+
+export const createBook = async (req, res) => {
+  const post = req.body;
+  const newBook = new Book(post);
+
+  try {
+    await newBook.save();
+    res.status(201).json(newBook);
+  } catch (err) {
+    res.status(409).json({ message: err.message })
+  }
+}
--- server/routes/books.js
+++ server/routes/books.js
@@ -1,9 +1,11 @@
 import express from 'express';
 
-import { getBooks } from '../controllers/books.js';
+import { getBooks, createBook } from '../controllers/books.js';
 
 const router = express.Router();
 
 router.get('/', getBooks);
+router.get('/', createBook);
+
 
 export default router;
