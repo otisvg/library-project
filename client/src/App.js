77529fd8da070c4b86d8f880c22070508a606a54
Otis <otisvickersgraver@gmail.com>
2021-02-11 23:14:56 +0000

    Render Books and Form in Material UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,33 @@
 import React from 'react';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
 
-const App = () => {
+import readify from './images/readify.png'
+import Books from './components/Books/Books'
+import Form from './components/Form/Form'
+
+
+const App = () =>  {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
-  )
+    <Container maxwidth="lg">
+        <AppBar position="static" color="inherit">
+          <Typography variant="h2" align="center"></Typography>
+          <img src={ readify } alt="readify" width="25%" height="auto"/>
+       </AppBar>
+       <Grow in>
+          <Container>
+            <Grid container justify="space-between" align-items="stretch" spacing={3}>
+                <Grid item xs={12} sm={7}>
+                  <Books />
+                </Grid>
+                <Grid item xs={12} sm={4}>
+                  <Form />
+                </Grid>
+            </Grid>
+          </Container>
+       </Grow>
+    </Container>
 
+  )
 }
 
 export default App;
