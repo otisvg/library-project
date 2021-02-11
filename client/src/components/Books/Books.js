c680b48afabc75ff66f1cb65d5460aab3fbb1edc
Otis <otisvickersgraver@gmail.com>
2021-02-11 23:14:12 +0000

    Render multiple Book components

---
+++ client/src/components/Books/Books.js
@@ -0,0 +1,16 @@
+ import React from 'react';
+ import Book from './Book/Book';
+
+ const Books = () => {
+    return ( 
+      <>
+      <h1>Books</h1>
+        <Book/>
+        <Book/>
+        <Book/>
+        <Book/>
+      </>
+    )
+ }
+
+ export default Books;
